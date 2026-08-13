ed223a9e8a08c02f68629a2cc6c21c6c8aff9842
marcio720roberto-dot <marcio720roberto@gmail.com>
2026-08-13 16:34:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function mostraform() {
+    const form = document.querySelector(".meu-formulario");
+    const mascara = document.querySelector(".mascara-meu-formulario");
+
+    if (form && mascara) {
+        form.classList.add("ativo");
+        mascara.classList.add("ativo");
+    }
+}
+
+function fecharForm() {
+    const form = document.querySelector(".meu-formulario");
+    const mascara = document.querySelector(".mascara-meu-formulario");
+
+    if (form && mascara) {
+        form.classList.remove("ativo");
+        mascara.classList.remove("ativo");
+    }
+} function mostraform() {
+    const form = document.querySelector(".meu-formulario");
+    const mascara = document.querySelector(".mascara-meu-formulario");
+
+    if (form && mascara) {
+        form.classList.add("ativo");
+        mascara.classList.add("ativo");
+    }
+}
+
+function fecharForm() {
+    const form = document.querySelector(".meu-formulario");
+    const mascara = document.querySelector(".mascara-meu-formulario");
+
+    if (form && mascara) {
+        form.classList.remove("ativo");
+        mascara.classList.remove("ativo");
+    }
+}
